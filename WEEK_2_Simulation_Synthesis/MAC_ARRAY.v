f97ce0abed9e1ca67d47d5f5dd832f73e682e1ad
hemanthnarava2003-netizen <hemanthnarava2003@gmail.com>
2025-11-22 11:46:16 +0530

    Update MAC_ARRAY.v

--- WEEK_2_Simulation_Synthesis/MAC_ARRAY.v
+++ WEEK_2_Simulation_Synthesis/MAC_ARRAY.v
@@ -1,1 +1,33 @@
-
+module mac_array #( 
+    parameter WIDTH = 16, 
+    parameter ACC_WIDTH = 32, 
+    parameter NUM_COL = 4 
+)( 
+    input  logic clk, 
+    input  logic rst_n, 
+    input  logic en, 
+    input  var logic signed [WIDTH-1:0] a [NUM_COL], 
+    input  var logic signed [WIDTH-1:0] b [NUM_COL], 
+    output var logic signed [ACC_WIDTH-1:0] y [NUM_COL] 
+); 
+ 
+    logic signed [ACC_WIDTH-1:0] acc [NUM_COL]; 
+    logic signed [2*WIDTH-1:0] product [NUM_COL]; 
+ 
+    integer i; 
+    always_ff @(posedge clk) begin 
+        if (!rst_n) begin 
+            for (i = 0; i < NUM_COL; i = i + 1) begin 
+                acc[i] <= '0; 
+            end 
+        end else if (en) begin 
+            for (i = 0; i < NUM_COL; i = i + 1) begin 
+                product[i] = a[i] * b[i]; 
+                acc[i] <= acc[i] + $signed(product[i]); 
+            end 
+        end 
+    end 
+ 
+    assign y = acc; 
+ 
+endmodule 
